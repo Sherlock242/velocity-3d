ae4840fb6901094abde1b2e2a8def155857603ed
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-02 19:41:40 +0000

    Increase space between light pink mandir and satsang building

--- src/components/game/world/track.ts
+++ src/components/game/world/track.ts
@@ -225,7 +225,7 @@ export function createGridAndScenery(
         lightMandir.position.set(specialBuildingX, 0, currentZ);
         gridGroup.add(lightMandir);
         staticCollidersRef.current.push(lightMandir);
-        currentZ += 80;
+        currentZ += 150;
 
         const satsangBuilding = createSatsangBuilding();
         satsangBuilding.position.set(specialBuildingX, 0, currentZ);
