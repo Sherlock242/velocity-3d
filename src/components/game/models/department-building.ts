e41944dbfaa37876b22ad05fb820b644366f07b7
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-02 16:23:45 +0000

    Doors are not completly center

--- src/components/game/models/department-building.ts
+++ src/components/game/models/department-building.ts
@@ -70,10 +70,9 @@ export function createDepartmentBuilding() {
       metalness: 0.8
   });
   const door = new THREE.Mesh(doorGeom, doorMaterial);
-  door.position.set(0, 0, 1.1);
+  door.position.set(0, 0, 1.01); // Position it relative to the entrance, slightly forward
   entrance.add(door);
 
 
   return building;
 }
-
