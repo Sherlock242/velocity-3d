0a2a6af52e9e110aabfd95fe903cc0e3dd1f19f4
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-02 18:43:56 +0000

    Black things showing in sky why

--- src/components/game/game-wrapper.tsx
+++ src/components/game/game-wrapper.tsx
@@ -166,7 +166,7 @@ export default function GameWrapper() {
 
 
     // --- SKYBOX ---
-    const skyGeometry = new THREE.BoxGeometry(8000, 8000, 8000);
+    const skyGeometry = new THREE.BoxGeometry(4900, 4900, 4900);
     const vertexShader = `
       varying vec3 vWorldPosition;
       void main() {
