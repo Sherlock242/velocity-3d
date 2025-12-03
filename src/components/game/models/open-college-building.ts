5a8bbefcaddf1d49ae30814e2cb8b5d3851f16f8
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-03 19:04:11 +0000

    Reduce C widerness

--- src/components/game/models/open-college-building.ts
+++ src/components/game/models/open-college-building.ts
@@ -5,7 +5,7 @@ export function createOpenCollegeBuilding() {
   const collegeGroup = new THREE.Group();
   collegeGroup.name = 'openCollegeBuildingWrapper';
 
-  const longWingWidth = 350;
+  const longWingWidth = 300;
   const shortWingWidth = 350;
   const wingDepth = 30;
   const numFloors = 1; // Only one story
