4e29abee3000605bda11263e2ca0152c763c68c8
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-03 17:41:56 +0000

    Put college building position top left corner

--- src/components/game/world/track.ts
+++ src/components/game/world/track.ts
@@ -212,7 +212,7 @@ export function createGridAndScenery(
         // --- College Building ---
         const college = createCollegeBuilding();
         college.scale.set(0.6, 0.6, 0.6);
-        college.position.set(0, 0, -plotDepth/2 + 50);
+        college.position.set(-plotWidth / 4, 0, -plotDepth / 4);
         campusContainer.add(college);
         const mainBuilding = college.getObjectByName('collegeBuilding');
         if (mainBuilding) {
