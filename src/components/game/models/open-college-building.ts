84e0624e6bd9202069f03b9886d44f410447c1b2
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-04 09:45:20 +0000

    More down

--- src/components/game/models/open-college-building.ts
+++ src/components/game/models/open-college-building.ts
@@ -138,7 +138,7 @@ export function createOpenCollegeBuilding() {
   const stageGeom = new THREE.BoxGeometry(stageWidth, stageHeight, stageDepth);
   const stage = new THREE.Mesh(stageGeom, stageMaterial);
   stage.position.y = stageHeight / 2;
-  stage.position.z = 120; // Center the stage in the courtyard
+  stage.position.z = 180; // Center the stage in the courtyard
   stage.castShadow = true;
   stage.receiveShadow = true;
   college.add(stage);
