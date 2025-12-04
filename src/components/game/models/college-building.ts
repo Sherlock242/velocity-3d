2d26ecf3d3b8a09ab33439979225fb81a511e817
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-04 20:15:33 +0000

    Just create a ramp that connects college building from 2nd floor to 3rd

--- src/components/game/models/college-building.ts
+++ src/components/game/models/college-building.ts
@@ -253,8 +253,22 @@ export function createCollegeBuilding() {
   platform2.position.x = platform1.position.x;
   platform2.position.z = platform1.position.z - rampLength - platformDepth;
 
+  // Ramp from 2nd to 3rd floor
+  const ramp3 = new THREE.Mesh(rampGeom, rampMaterial);
+  ramp3.position.y = rampRise * 2 + rampRise / 2;
+  ramp3.position.x = platform2.position.x - (platformWidth - rampWidth) / 2;
+  ramp3.position.z = platform2.position.z + platformDepth / 2 - rampLength / 2;
+  ramp3.rotation.x = -Math.atan(rampRise / rampLength);
+
+  // Platform on the 3rd floor
+  const platform3Geom = new THREE.BoxGeometry(platformWidth, rampThickness, platformDepth);
+  const platform3 = new THREE.Mesh(platform3Geom, rampMaterial);
+  platform3.position.y = rampRise * 3;
+  platform3.position.x = platform1.position.x;
+  platform3.position.z = platform1.position.z;
+
   const walkableRampGroup = new THREE.Group();
-  walkableRampGroup.add(ramp1, platform1, ramp2, platform2);
+  walkableRampGroup.add(ramp1, platform1, ramp2, platform2, ramp3, platform3);
   walkableRampGroup.name = 'collegeRamp';
   
   rampGroup.add(walkableRampGroup);
