4576d0432d7079d58507209d8d3907822b0423ec
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-04 13:42:03 +0000

    Extend 2 morr

--- src/components/game/world/sectors/sector-10.ts
+++ src/components/game/world/sectors/sector-10.ts
@@ -110,29 +110,29 @@ export function createSector10({
 
   // Walkable Path to College
   const pathWidth = 15;
-  const pathToCollegeLength = 105;
+  const pathToCollegeLength = 112;
   const pathToCollegeGeom = new THREE.PlaneGeometry(pathToCollegeLength, pathWidth);
   const pathToCollege = new THREE.Mesh(pathToCollegeGeom, pathMaterial);
   pathToCollege.rotation.x = -Math.PI / 2;
-  pathToCollege.position.set(gateXPosition - (pathToCollegeLength / 2) - 12.5, 0.15, frontWallZ + entranceRoadLength - pathWidth / 2 - 6);
+  pathToCollege.position.set(gateXPosition - (pathToCollegeLength / 2) - 12.5 - 1, 0.15, frontWallZ + entranceRoadLength - pathWidth / 2 - 8);
   campusContainer.add(pathToCollege);
   
   const corridorLength = 40;
   const corridorGeom = new THREE.PlaneGeometry(pathWidth, corridorLength);
   const corridor = new THREE.Mesh(corridorGeom, pathMaterial);
   corridor.rotation.x = -Math.PI / 2;
-  corridor.position.set(gateXPosition - 100 - 20, 0.15, frontWallZ + entranceRoadLength - pathWidth - (corridorLength / 2) + 4);
+  corridor.position.set(gateXPosition - 100 - 20, 0.15, frontWallZ + entranceRoadLength - pathWidth - (corridorLength / 2) + 10 - 2);
   campusContainer.add(corridor);
 
 
   // --- Sheds for Walkways ---
   const pathToCollegeShed = createWalkwayShed(pathToCollegeLength, pathWidth);
-  pathToCollegeShed.position.set(gateXPosition - (pathToCollegeLength / 2) - 12.5, 0, frontWallZ + entranceRoadLength - pathWidth / 2 - 6);
+  pathToCollegeShed.position.set(gateXPosition - (pathToCollegeLength / 2) - 12.5 - 1, 0, frontWallZ + entranceRoadLength - pathWidth / 2 - 8);
   pathToCollegeShed.rotation.y = Math.PI / 2;
   campusContainer.add(pathToCollegeShed);
 
   const corridorShed = createWalkwayShed(corridorLength, pathWidth);
-  corridorShed.position.set(gateXPosition - 100 - 20, 0, frontWallZ + entranceRoadLength - pathWidth - (corridorLength / 2) + 4);
+  corridorShed.position.set(gateXPosition - 100 - 20, 0, frontWallZ + entranceRoadLength - pathWidth - (corridorLength / 2) + 10 - 2);
   campusContainer.add(corridorShed);
 
 
