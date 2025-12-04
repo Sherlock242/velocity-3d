4a0e598bedd0522502699271914edc0ace4bdaa7
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-04 13:48:47 +0000

    Corner of walkable became because of shed walls crossing each other on 9

--- src/components/game/world/sectors/sector-10.ts
+++ src/components/game/world/sectors/sector-10.ts
@@ -110,29 +110,37 @@ export function createSector10({
 
   // Walkable Path to College
   const pathWidth = 15;
-  const pathToCollegeLength = 112;
+  const corridorLength = 40;
+  
+  const pathToCollegeLength = 112 - (corridorLength / 2) + 2.5;
+  const pathToCollegeX = gateXPosition - (pathToCollegeLength / 2) - 12.5;
+  const pathToCollegeZ = frontWallZ + entranceRoadLength - pathWidth / 2;
+
+
   const pathToCollegeGeom = new THREE.PlaneGeometry(pathToCollegeLength, pathWidth);
   const pathToCollege = new THREE.Mesh(pathToCollegeGeom, pathMaterial);
   pathToCollege.rotation.x = -Math.PI / 2;
-  pathToCollege.position.set(gateXPosition - (pathToCollegeLength / 2) - 12.5 - 1, 0.15, frontWallZ + entranceRoadLength - pathWidth / 2 - 8);
+  pathToCollege.position.set(pathToCollegeX, 0.15, pathToCollegeZ);
   campusContainer.add(pathToCollege);
   
-  const corridorLength = 40;
+  const corridorX = pathToCollegeX - (pathToCollegeLength / 2) - (pathWidth / 2);
+  const corridorZ = pathToCollegeZ - pathWidth - (corridorLength / 2) + 12;
+
   const corridorGeom = new THREE.PlaneGeometry(pathWidth, corridorLength);
   const corridor = new THREE.Mesh(corridorGeom, pathMaterial);
   corridor.rotation.x = -Math.PI / 2;
-  corridor.position.set(gateXPosition - 100 - 20, 0.15, frontWallZ + entranceRoadLength - pathWidth - (corridorLength / 2) + 10 - 2);
+  corridor.position.set(corridorX, 0.15, corridorZ);
   campusContainer.add(corridor);
 
 
   // --- Sheds for Walkways ---
   const pathToCollegeShed = createWalkwayShed(pathToCollegeLength, pathWidth);
-  pathToCollegeShed.position.set(gateXPosition - (pathToCollegeLength / 2) - 12.5 - 1, 0, frontWallZ + entranceRoadLength - pathWidth / 2 - 8);
+  pathToCollegeShed.position.set(pathToCollegeX, 0, pathToCollegeZ);
   pathToCollegeShed.rotation.y = Math.PI / 2;
   campusContainer.add(pathToCollegeShed);
 
   const corridorShed = createWalkwayShed(corridorLength, pathWidth);
-  corridorShed.position.set(gateXPosition - 100 - 20, 0, frontWallZ + entranceRoadLength - pathWidth - (corridorLength / 2) + 10 - 2);
+  corridorShed.position.set(corridorX, 0, corridorZ);
   campusContainer.add(corridorShed);
 
 
