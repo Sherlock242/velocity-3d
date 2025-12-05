d426c49a0cf993d64680ef72e2ea71b4e50f15ad
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-05 05:03:55 +0000

    Move ramp 3 slightly in front

--- src/components/game/models/college-building.ts
+++ src/components/game/models/college-building.ts
@@ -258,7 +258,7 @@ export function createCollegeBuilding() {
   const ramp3 = new THREE.Mesh(ramp3Geom, rampMaterial);
   ramp3.position.y = rampRise * 2.5; // Midpoint for 3rd floor ramp
   ramp3.position.x = platform2.position.x - 30;
-  ramp3.position.z = platform2.position.z + rampLength / 2 + 10;
+  ramp3.position.z = platform2.position.z + rampLength / 2 + 20;
   ramp3.rotation.x = -Math.atan(rampRise / rampLength);
 
 
