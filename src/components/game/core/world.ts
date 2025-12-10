be25e0b8f85f3b19daee6a7ea08b56593f669644
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-10 17:24:02 +0000

    More 90 degrer

--- src/components/game/core/world.ts
+++ src/components/game/core/world.ts
@@ -25,7 +25,7 @@ export function createWorld(scene: THREE.Scene, theme: TrackTheme, gameState: Ga
 
     transformer.position.x = cellCenterX;
     transformer.position.z = cellCenterZ;
-    transformer.rotation.y = Math.PI / 2;
+    transformer.rotation.y = Math.PI;
     
     scene.add(transformer);
     playerRef.current = transformer;
