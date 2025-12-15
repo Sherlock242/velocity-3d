a617a8913c1989fef6b0150746fbd80e281f6a6e
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-15 14:34:10 +0000

    The main road roads of sector 25-24 are fluctuating

--- src/components/game/world/track.ts
+++ src/components/game/world/track.ts
@@ -75,7 +75,7 @@ export function createGridAndScenery(
     }
     
     // Horizontal roads - Don't render road for the last row (dome area)
-    if (i < GRID_SIZE) {
+    if (i < GRID_SIZE - 1) {
         const horizontalRoadGeom = new THREE.PlaneGeometry(TOTAL_GRID_WIDTH, ROAD_WIDTH);
         const horizontalRoad = new THREE.Mesh(horizontalRoadGeom, roadMaterial);
         horizontalRoad.rotation.x = -Math.PI / 2;
