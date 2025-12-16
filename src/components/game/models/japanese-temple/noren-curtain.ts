c55519f0710270ee9fe773fa381e4b3481be1464
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-16 21:41:33 +0000

    Lines and black circle is fluctuating

--- src/components/game/models/japanese-temple/noren-curtain.ts
+++ src/components/game/models/japanese-temple/noren-curtain.ts
@@ -28,7 +28,7 @@ export function createNorenCurtain() {
         const lineGeom = new THREE.BoxGeometry(redLineWidth, panelHeight, 0.1);
         const line = new THREE.Mesh(lineGeom, darkRedMaterial);
         line.position.x = currentX + redLineWidth / 2;
-        line.position.z = 0.05;
+        line.position.z = 0.1;
         norenGroup.add(line);
         currentX += redLineWidth;
     };
@@ -39,7 +39,7 @@ export function createNorenCurtain() {
         const circleGeom = new THREE.CircleGeometry(circleRadius, 32);
         const circle = new THREE.Mesh(circleGeom, blackMaterial);
         circle.position.x = xPos;
-        circle.position.z = 0.1;
+        circle.position.z = 0.2;
         norenGroup.add(circle);
     }
 
