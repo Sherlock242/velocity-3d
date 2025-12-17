81221564e73c97dfa5d001359dc58cfba991319c
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-17 14:26:07 +0000

    Increase thickness of curtain red vertical line too 1

--- src/components/game/models/japanese-temple/noren-curtain.ts
+++ src/components/game/models/japanese-temple/noren-curtain.ts
@@ -20,7 +20,7 @@ export function createNorenCurtain() {
     });
 
     const panelHeight = 20;
-    const redLineWidth = 0.2;
+    const redLineWidth = 1;
     const emblemPanelWidth = 11;
     const gapPanelWidth = 8;
     const endPanelWidth = 5.5; // Half of emblem panel
