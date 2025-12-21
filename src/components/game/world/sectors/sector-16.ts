f2d982b577c4ef2637ffa4ce86f0df1699888e3c
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-21 19:51:21 +0000

    Create one simple house in sector 16

--- src/components/game/world/sectors/sector-16.ts
+++ src/components/game/world/sectors/sector-16.ts
@@ -16,16 +16,10 @@ export function createSector16({
 }: Sector16Props): THREE.Group {
   const sectorGroup = new THREE.Group();
 
-  const house1 = createSimpleHouse();
-  house1.position.set(cellCenterX - 50, 0, cellCenterZ - 50);
-  sectorGroup.add(house1);
-  staticCollidersRef.current.push(house1);
-
-  const house2 = createSimpleHouse();
-  house2.position.set(cellCenterX + 50, 0, cellCenterZ + 50);
-  house2.rotation.y = Math.PI / 2;
-  sectorGroup.add(house2);
-  staticCollidersRef.current.push(house2);
+  const house = createSimpleHouse();
+  house.position.set(cellCenterX, 0, cellCenterZ);
+  sectorGroup.add(house);
+  staticCollidersRef.current.push(house);
 
   return sectorGroup;
 }
