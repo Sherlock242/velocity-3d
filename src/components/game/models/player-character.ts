615ce67473ddbc33b61b85c68c8a90d560c83713
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-21 14:32:42 +0000

    Reduce Y Position of of overall hair

--- src/components/game/models/player-character.ts
+++ src/components/game/models/player-character.ts
@@ -75,7 +75,7 @@ export function createPlayerCharacter(
   // Hair
   const hairMaterial = new THREE.MeshStandardMaterial({ color: 0x080808, roughness: 0.8 });
   const hair = createHair(headRadius, hairMaterial);
-  hair.position.y = headHeight/2 + 0.1;
+  hair.position.y = headHeight/2 - 0.1;
   head.add(hair);
 
   // Torso and Shirt with hexagonal shape
