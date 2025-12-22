7b2a5f344ef5362129aa9f48e192ac88fac59f1d
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-22 12:12:53 +0000

    Keep one small house in front of sector 16

--- src/components/game/world/sectors/sector-16.ts
+++ src/components/game/world/sectors/sector-16.ts
@@ -2,6 +2,7 @@
 import * as THREE from 'three';
 import { createSimpleHouse } from '../../models/simple-house';
 import type { MutableRefObject } from 'react';
+import { ROAD_WIDTH } from '@/lib/game-constants';
 
 type Sector16Props = {
   cellCenterX: number;
@@ -20,6 +21,15 @@ export function createSector16({
   house.position.set(cellCenterX, 0, cellCenterZ);
   sectorGroup.add(house);
   staticCollidersRef.current.push(house);
+
+  const frontHouse = createSimpleHouse();
+  // Position it along the road in front of the sector
+  const roadZ = cellCenterZ + 500 - ROAD_WIDTH / 2 - 50; // Place it before the next road
+  frontHouse.position.set(cellCenterX, 0, roadZ);
+  frontHouse.rotation.y = Math.PI;
+  sectorGroup.add(frontHouse);
+  staticCollidersRef.current.push(frontHouse);
+
 
   return sectorGroup;
 }
