31c5972a492f3012c888fa08f21842ec7b8883ed
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-22 19:25:59 +0000

    Create one simple house in sector 16

--- src/components/game/world/sectors/sector-16.ts
+++ src/components/game/world/sectors/sector-16.ts
@@ -1,4 +1,3 @@
-
 import * as THREE from 'three';
 import { createSimpleHouse } from '../../models/simple-house';
 import type { MutableRefObject } from 'react';
@@ -21,15 +20,6 @@ export function createSector16({
   house.position.set(cellCenterX, 0, cellCenterZ);
   sectorGroup.add(house);
   staticCollidersRef.current.push(house);
-
-  const frontHouse = createSimpleHouse();
-  // Position it along the road in front of the sector
-  const roadZ = cellCenterZ + 500 - ROAD_WIDTH / 2 - 50; // Place it before the next road
-  frontHouse.position.set(cellCenterX, 0, roadZ);
-  frontHouse.rotation.y = Math.PI;
-  sectorGroup.add(frontHouse);
-  staticCollidersRef.current.push(frontHouse);
-
 
   return sectorGroup;
 }
