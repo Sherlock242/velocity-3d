ffee60955c80a750d1abbb6de9da793d23589c7c
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-23 04:43:34 +0000

    Change sky box shape to circle

--- src/components/game/core/scene.ts
+++ src/components/game/core/scene.ts
@@ -18,7 +18,7 @@ export function setupScene(mountNode: HTMLDivElement) {
     camera.add(audioListener);
 
     // Skybox
-    const skyGeometry = new THREE.BoxGeometry(5500, 5500, 5500);
+    const skyGeometry = new THREE.SphereGeometry(3000, 32, 16);
     const vertexShader = `
       varying vec3 vWorldPosition;
       void main() {
