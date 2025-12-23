063ddf1ccd4afbe9f14def7bbc65ae776495da36
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-23 12:45:28 +0000

    I'm still able to zoom in and out, and there is still scoll of browser

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark overflow-hidden">
+    <html lang="en" className="dark">
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -26,7 +26,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased overflow-hidden no-scrollbar">
+      <body className="font-body antialiased">
         {children}
         <Toaster />
       </body>
