c5978e97a6935d8cf61d8b2a329cb9d65bf5d449
Sherlock Homes <sherlockhomes844@gmail.com>
2025-12-25 11:01:52 +0000

    Create one simple house in sector 3

--- src/components/game/world/sectors/sector-3.ts
+++ src/components/game/world/sectors/sector-3.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { createSimpleHouse } from '../../models/simple-house';
 import type { MutableRefObject } from 'react';
+import { ROAD_WIDTH } from '@/lib/game-constants';
 
 type Sector3Props = {
   cellCenterX: number;
@@ -19,6 +20,12 @@ export function createSector3({
   house.position.set(cellCenterX, 0, cellCenterZ);
   sectorGroup.add(house);
   staticCollidersRef.current.push(house);
+
+  const house2 = createSimpleHouse();
+  house2.position.set(cellCenterX + ROAD_WIDTH * 2, 0, cellCenterZ - ROAD_WIDTH);
+  house2.rotation.y = Math.PI / 3;
+  sectorGroup.add(house2);
+  staticCollidersRef.current.push(house2);
 
   return sectorGroup;
 }
